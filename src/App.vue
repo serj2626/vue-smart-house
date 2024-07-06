03c07c39c77855c2c12eecace70807ccf95cb164
serj2626 <serj2626@mail.ru>
2024-07-07 01:00:25 +0300

    new text in app.vue

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,15 @@ import Navigation from './components/Navigation.vue'
 </script>
 
 <template>
-  <header>
-    <Navigation />
-  </header>
-  <main>
-    <RouterView />
-  </main>
+  <div class="app container">
+    <header>
+      <Navigation />
+    </header>
+    <main>
+      <RouterView />
+    </main>
+  </div>
+
 
 </template>
 
