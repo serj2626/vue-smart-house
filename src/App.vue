99347d852cdee78c9eabd5ab2c9f2423287e7d76
serj2626 <serj2626@mail.ru>
2024-07-07 00:44:24 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navigation from './components/Navigation.vue'
+</script>
+
+<template>
+  <header>
+    <Navigation />
+  </header>
+  <main>
+    <RouterView />
+  </main>
+
+</template>
+
+<style scoped></style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="flex justify-end items-center">
+    <img src="../assets/images/status/svet_off.png" alt="">
+    <img src="../assets/images/status/svet_on.png" alt="">
+  </nav>
+  <header class="container h-[100px]  
+  flex justify-center items-center bg-slate-300">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+  </header>
+
+</template>
+
+<style scoped>
+nav {
+  height: 60px;
+  background-color: black;
+}
+
+nav img {
+  height: 40px;
+  margin-right: 10px;
+  margin-left: 10px;
+  width: auto;
+}
+</style>
