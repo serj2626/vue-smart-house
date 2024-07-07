04d307fb0316a24856543d478676e4d62572eb27
serj2626 <serj2626@mail.ru>
2024-07-07 17:15:57 +0300

    update file README

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,21 +3,27 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <nav class="flex justify-end items-center">
-    <img src="../assets/images/status/svet_off.png" alt="">
-    <div>
-      <p class="text-white">
-        26 <span>&#176;</span>
-      </p>
-    </div>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-      class="size-6 text-white w-8 h-auto">
-      <path stroke-linecap="round" stroke-linejoin="round"
-        d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
-    </svg>
+  <nav class="flex justify-around items-center">
 
-    <img src="../assets/images/status/svet_on.png" alt="">
+    <div>
+      <p class="text-white font-serif font-bold text-2xl">Умный дом</p>
+    </div>
+    <div class="flex justify-end items-center gap-2">
+      <div>
+        <p class="text-white">
+          26 <span>&#176;</span>
+        </p>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        class="size-4 text-white w-8 h-auto">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+      </svg>
+    </div>
+
   </nav>
+
+
   <header class="container mx-auto h-[100px] mt-3 flex justify-around items-center">
     <img src="/logo1.png" width="100" height="200" alt="Мой Дом">
     <div class="flex">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,11 +26,23 @@ onMounted(getInstruments);
   <h1>Управление техникой</h1>
 
   <div class="flex justify-around flex-wrap">
-    <div v-for="item in instruments" :key="item.id"
-      class="card max-w-sm px-6 pb-10 pt-2  bg-white border border-gray-200 rounded-lg shadow
-       dark:bg-gray-800 dark:border-gray-700"
-    >
-       <div class="title">{{ item.name }}</div>
+    <div v-for="item in instruments" :key="item.id" class="card max-w-sm px-6 pb-10 pt-2  bg-white border border-gray-200
+     rounded-lg shadow
+       dark:bg-gray-800 dark:border-gray-700">
+      <div class="flex justify-between items-center py-4">
+        <div class="title">
+          {{ item.name }}
+        </div>
+        <div v-if="item.status">
+          <button class="on">Вкл</button>
+        </div>
+        <div v-else>
+          <button class="off">Выкл</button>
+        </div>
+      </div>
+      <hr>
+      <p class="type"><span class="py-1 px-2 bg-slate-300 rounded-md">{{ item.type }} {{ item.value }}</span></p>
+
       <a href="#">
         <img class="image rounded-t-lg w-full h-80" :src="item.image" alt="" />
       </a>
@@ -46,20 +58,45 @@ h1 {
   color: rgb(67, 53, 53);
 }
 
-.card{
-  margin-bottom: 20px;
+.type {
+  padding-block: 10px;
+  text-align: end;
+  font-size: 20px;
+  font-weight: bold;
+  color: rgb(67, 53, 53);
 }
-.title{
+
+.card {
+  margin-bottom: 20px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.title {
   text-align: center;
   font-size: 20px;
   font-weight: bold;
   color: rgb(149, 4, 4);
 }
 
-.image{
+.image {
   transition: 0.7s;
 }
-.image:hover{
+
+.image:hover {
   transform: translateX(-15px);
+}
+
+.on {
+  background-color: green;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+}
+
+.off{
+  background-color: red;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
 }
 </style>
