6f07b04e54ec4c6b328194c604ddf26e437a4f65
serj2626 <serj2626@mail.ru>
2024-07-08 03:19:05 +0300

    updated size icon in footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p class="text-red-700"><span class="font-bold">Boytsov SV</span> &copy; 2024 St-Petersburg</p>
+    </div>
+    <div class="flex gap-4">
+        <i class="fa-brands fa-vk fa-2xl" style="color: #024bca;"></i>
+        <i class="fa-brands fa-linkedin fa-2xl" style="color: #419ce1;"></i>
+        <i class="fa-brands fa-github fa-2xl"></i>
+        <i class="fa-brands fa-telegram fa-2xl" style="color: #0f7dd2;"></i>
+    </div>
+</template>
+
+<style scoped>
+i:hover{
+    cursor: pointer;
+}
+</style>
